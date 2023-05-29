95083b454278e4ef9ecaf435c496482a9fa7f71e
domenicatonziello <domenica.tonziello10@gmail.com>
2023-05-29 17:00:14 +0200

    create

--- src/main/java/org/java/pizzeria/demo/controller/PizzaController.java
+++ src/main/java/org/java/pizzeria/demo/controller/PizzaController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,5 +50,17 @@ public class PizzaController {
 		model.addAttribute("pizza",pizza);
 		
 		return "show";
+	}
+	
+	@GetMapping("/pizza/create")
+	public String create() {
+		return "create";
+	}
+	@PostMapping("pizza/create")
+	public String storeBook(@ModelAttribute Pizza pizza) {
+
+		pizzarepo.save(pizza);
+
+		return "redirect:/";
 	}
 }
